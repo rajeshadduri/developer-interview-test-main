654a0f1718f2191e89763c4ed649b362e4346e0b
rajeshadduri@gmail.com <rajeshadduri@gmail.com>
2024-11-28 19:59:55 +0000

    Extracted interfaces

---
+++ Smartwyre.DeveloperTest/Interfaces/IProductDataStore.cs
@@ -0,0 +1,9 @@
+﻿using Smartwyre.DeveloperTest.Types;
+
+namespace Smartwyre.DeveloperTest.Interfaces
+{
+    public interface IProductDataStore
+    {
+        Product GetProduct(string productIdentifier);
+    }
+}
---
+++ Smartwyre.DeveloperTest/Interfaces/IRebateDataStore.cs
@@ -0,0 +1,10 @@
+﻿using Smartwyre.DeveloperTest.Types;
+
+namespace Smartwyre.DeveloperTest.Interfaces
+{
+    public interface IRebateDataStore
+    {
+        Rebate GetRebate(string rebateIdentifier);
+        void StoreCalculationResult(Rebate account, decimal rebateAmount);
+    }
+}
